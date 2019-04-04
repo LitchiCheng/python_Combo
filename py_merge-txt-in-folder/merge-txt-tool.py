bcb9d413d9d450b7b270e304e26cf280b856420a
darboy <974782852@qq.com>
2019-04-04 13:45:35 +0800

    add merge-txt-tool.py

---
+++ py_merge-txt-in-folder/merge-txt-tool.py
@@ -0,0 +1,21 @@
+#coding=utf-8 
+import re
+import sys,os
+from os import path
+address = sys.argv[1]
+address = sys.argv[1].replace('\\','\\\\')
+#获取目标文件夹的路径
+filedir = address
+#获取当前文件夹中的文件名称列表  
+filenames=os.listdir(filedir)
+#打开当前目录下的result.txt文件，如果没有则创建
+f=open('merge_result.txt','w')
+#先遍历文件名
+for filename in filenames:
+    filepath = filedir+'/'+filename
+    #遍历单个文件，读取行数
+    for line in open(filepath):
+        f.writelines(line)
+    # f.write('\n')
+#关闭文件
+f.close()
