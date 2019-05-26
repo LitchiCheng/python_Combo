e9cf284fd09691153a397b4d43b40af408e628d5
darboy <974782852@qq.com>
2019-05-26 21:35:08 +0800

    package the random_odd_list to a function

--- py_random_odd_number/py_random_odd_number.py
+++ py_random_odd_number/py_random_odd_number.py
@@ -1,7 +1,15 @@
 import random
-odd_random_list = [0] * 12      # 12 is the len of list
-# the first method
-odd_random_list = [random.randint(0, 50) * 2 + 1 for i in range(12)]      #2n+1, the 50 is the range of odd number
-print(odd_random_list)
-odd_random_list = random.sample([i for i in range(100) if i % 2 == 1], 12)          #list the odd number of a range, then sample the number 
-print(odd_random_list)
+# odd_random_list = [0] * 12      # 12 is the len of list
+# # the first method
+# odd_random_list = [random.randint(0, 50) * 2 + 1 for i in range(12)]      #2n+1, the 50 is the range of odd number
+# print(odd_random_list)
+# odd_random_list = random.sample([i for i in range(100) if i % 2 == 1], 12)          #list the odd number of a range, then sample the number 
+# print(odd_random_list)
+
+def returnOddIntListOfRandom(range_start, range_end, quantity):
+    odd_random_list = [0] * quantity
+    odd_random_list = random.sample([i for i in range(100) if i % 2 == 1], 12)
+    return odd_random_list  
+
+while(1):
+    print(returnOddIntListOfRandom(0,100,12))
