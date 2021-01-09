d4d28e71b5e68395b4a7886389ab20e765d86d14
darboy <974782852@qq.com>
2021-01-09 17:53:12 +0800

    update

--- py_fetch-in-txt/src/__init__.py
+++ py_fetch-in-txt/src/__init__.py
@@ -11,9 +11,11 @@ f2 = open(out_address,'w+')
 count = 0
 key_word = input("输入关键字： ")
 for line in f1.readlines():
-    if re.findall(key_word ,line):                  #查找“key_word”的行
-        f2.write(line)                              #把查找到的行写入f2.
-        count = count + 1;
+    if re.findall(key_word ,line):
+        line = line.replace("[Text][Motors|motor2|", "[Linear][")
+
+        # print(line1)
+    f2.write(line) 
 f1.close()
 f2.close()
 print("找到的行数：",count)
